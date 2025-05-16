a331e2dfe779e1e3e56d1484535d64201d196f30
박지훈 <qkrwlgns156@naver.com>
2025-05-16 15:15:42 +0900

    종합

--- my-vue-project/src/components/YoutubeCard.vue
+++ my-vue-project/src/components/YoutubeCard.vue
@@ -1,11 +1,39 @@
 <template>
+  <div>
+    <img 
+      :src="video.snippet.thumbnails.medium.url" 
+      :alt="video.snippet.title" 
+      @click="openModal" 
 
+    />
+    <h3>{{ video.snippet.title }}</h3>
+    <p>{{ video.snippet.description }}</p>
+
+    <!-- 모달이 열리면 YoutubeReviewModal 렌더링 -->
+    <YoutubeReviewModal 
+      v-if="isModalOpen" 
+      :videoId="video.id.videoId"
+      :videotitle="video.snippet.title" 
+      @close="closeModal" 
+    />
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import YoutubeReviewModal from './YoutubeReviewModal.vue'
 
+const props = defineProps({
+  video: Object,
+})
+
+const isModalOpen = ref(false)
+
+function openModal() {
+  isModalOpen.value = true
+}
+
+function closeModal() {
+  isModalOpen.value = false
+}
 </script>
-
-<style scoped>
-
-</style>
--- my-vue-project/src/stores/youtube.js
+++ my-vue-project/src/stores/youtube.js
@@ -1,10 +1,33 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export const useYoutubeStore = defineStore('youtube', () => {
-  const youtubeList=ref([])
-  const addyoutube=function(list){
-    youtubeList.value=list
+  const youtubeList = ref([])
+
+  const addyoutube = (list) => {
+    youtubeList.value = list
   }
-  return { youtubeList,addyoutube }
+
+  const searchYoutube = (num, query) => {
+    const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY
+
+    axios.get('https://www.googleapis.com/youtube/v3/search', {
+      params: {
+        part: 'snippet',
+        q: `${query} 리뷰`,
+        type: 'video',
+        maxResults: num,
+        key: API_KEY,
+      },
+    })
+      .then((response) => {
+        addyoutube(response.data.items)
+      })
+      .catch((error) => {
+        console.error('YouTube API 요청 실패:', error)
+      })
+  }
+
+  return { youtubeList, addyoutube, searchYoutube }
 })
