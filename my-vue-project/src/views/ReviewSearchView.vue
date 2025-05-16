7784e3c1351d4faddba0ba70bb56b375bdbe8e7b
minhwan <minhwan2315@naver.com>
2025-05-16 14:52:49 +0900

    minhwan tw0 update

--- my-vue-project/src/views/ReviewSearchView.vue
+++ my-vue-project/src/views/ReviewSearchView.vue
@@ -1,42 +1,24 @@
 <template>
   <div>
     <input v-model="query" placeholder="영화 제목 입력" />
-    <button @click="searchYoutube">검색</button>
+    <button @click="onsearchYoutube">검색</button>
+    <YouTubeCard
+      v-for="video in store.youtubeList"
+      :key="video.id.videoId"
+      :video="video"
+    />
   </div>
 </template>
 
 <script setup>
-import { useYoutubeStore } from "@/stores/youtube.js"; // ✅ 오타 수정
-import { ref } from 'vue';
-import axios from 'axios';
+import { ref } from 'vue'
+import { useYoutubeStore } from '@/stores/youtube.js'
+import YouTubeCard from '@/components/YouTubeCard.vue'
 
-const store = useYoutubeStore();
-const query = ref(''); // 검색어
+const query = ref('')
+const store = useYoutubeStore()
 
-const searchYoutube = () => { // ✅ 함수 이름 오타 수정
-  const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
-
-  axios({
-    method: 'get',
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    params: {
-      part: 'snippet',
-      q: `${query.value} 리뷰`,
-      type: 'video',
-      maxResults: 10,
-      key: API_KEY,
-    },
-  })
-    .then((response) => {
-      // ✅ items로 수정
-      store.addyoutube(response.data.items);
-      console.log(store.youtubeList); // ref인지 reactive인지에 따라 .value 여부 다름
-    })
-    .catch((error) => {
-      console.error('YouTube API 요청 실패:', error);
-    });
-};
+const onsearchYoutube = () => {
+  store.searchYoutube(10, query.value)
+}
 </script>
-
-<style scoped>
-</style>
